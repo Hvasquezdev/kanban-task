dc3101412fee0796f0615a41396c21c0be8b81ef
Hector Vazquez <hvasquezdev@gmail.com>
2024-02-27 21:00:30 -0300

    CU-86a2d1qk8 - feat: add base select component

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,23 @@
 import { useState } from 'react';
 import reactLogo from './assets/react.svg';
-import viteLogo from './vite.svg';
 import './App.css';
+import BaseSelect from './components/BaseSelect';
+import { Option } from './components/BaseSelect/BaseSelect';
 
 function App() {
   const [count, setCount] = useState(0);
+  const [option, setOption] = useState<Option>();
+
+  const options = [
+    { value: 'chocolate', label: 'Chocolate' },
+    { value: 'strawberry', label: 'Strawberry' },
+    { value: 'vanilla', label: 'Vanilla' }
+  ];
 
   return (
     <>
+      <BaseSelect options={options} value={option} onSelect={setOption} />
       <div>
-        <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
         <a href="https://react.dev" target="_blank" rel="noreferrer">
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
