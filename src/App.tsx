b6490398e9e46e2e44c104227dc84560ce86e630
Angel Figuera <angeleraser@gmail.com>
2024-03-01 08:36:46 -0400

    CU-86a21ppe - feat: add BaseTextField component

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
 import { useState } from 'react';
-import reactLogo from './assets/react.svg';
 import './App.css';
 import BaseSelect from './components/BaseSelect';
 import { Option } from './components/BaseSelect/BaseSelect';
+import BaseTextField from './components/BaseTextField';
 
 function App() {
-  const [count, setCount] = useState(0);
   const [option, setOption] = useState<Option>();
+  const [text, setText] = useState('');
 
   const options = [
     { value: 'chocolate', label: 'Chocolate' },
@@ -17,21 +17,11 @@ function App() {
   return (
     <>
       <BaseSelect options={options} value={option} onSelect={setOption} />
-      <div>
-        <a href="https://react.dev" target="_blank" rel="noreferrer">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button type="button" onClick={() => setCount((currentCount) => currentCount + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">Click on the Vite and React logos to learn more</p>
+
+      <form>
+        <BaseTextField onChange={setText} value={text} label="Text Field" required placeholder="e.g Make cofee" />
+        <button type="submit">Submit</button>
+      </form>
     </>
   );
 }
