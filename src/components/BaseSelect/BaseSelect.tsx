1ef0c63eff94c110b3a768013b899e84838c102f
Niicklz <77895883+Niicklz@users.noreply.github.com>
2024-03-08 19:03:49 -0400

    CU-86a2fck5a - refactor: add label to base select component

--- src/components/BaseSelect/BaseSelect.tsx
+++ src/components/BaseSelect/BaseSelect.tsx
@@ -1,5 +1,7 @@
 import Select from 'react-select';
 import './BaseSelect.css';
+import { useId } from 'react';
+import BaseHeading from '../BaseHeading';
 
 export type Option = {
   value: string | number;
@@ -12,18 +14,31 @@ interface BaseSelectProps {
   options?: Option[];
   value?: Option;
   onSelect?: (option: Option) => void;
+  label?: string;
+  id?: string;
 }
 
-function BaseSelect({ options = [], className = '', placeholder, value, onSelect }: BaseSelectProps) {
+function BaseSelect({ options = [], className = '', placeholder, value, onSelect, label, id }: BaseSelectProps) {
+  const defaultId = useId();
+
   return (
-    <Select
-      placeholder={placeholder}
-      classNamePrefix="base-select"
-      className={`base-select ${className}`}
-      value={value}
-      options={options}
-      onChange={(option) => onSelect?.(option as Option)}
-    />
+    <label htmlFor={id ?? defaultId} className="base-select-container">
+      {label && (
+        <BaseHeading variant="h6" className="base-select-container__label">
+          {label}
+        </BaseHeading>
+      )}
+
+      <Select
+        inputId={id ?? defaultId}
+        placeholder={placeholder}
+        classNamePrefix="base-select"
+        className={`base-select ${className}`}
+        value={value}
+        options={options}
+        onChange={(option) => onSelect?.(option as Option)}
+      />
+    </label>
   );
 }
 
