f3415f4198950878366ce34d35f3ee98d6a6c186
Hector Vazquez <hvasquezdev@gmail.com>
2024-03-12 20:56:19 -0300

    CU-86a2fh58h - feat: add app base layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,72 +1,7 @@
-import { useState } from 'react';
-import BaseSelect from './components/BaseSelect';
-import { Option } from './components/BaseSelect/BaseSelect';
-import BaseTextField from './components/BaseTextField';
-import BaseMenu from './components/BaseMenu';
-import { MenuOptionColor } from './components/BaseMenu/BaseMenu';
-import BaseCheckbox from './components/BaseCheckbox';
-import BaseModal from './components/BaseModal';
-import BaseButton from './components/BaseButton';
-import BaseTextarea from './components/BaseTextarea';
+import BaseLayout from './layouts/BaseLayout';
 
 function App() {
-  const [option, setOption] = useState<Option>();
-  const [text, setText] = useState('');
-  const [description, setDescription] = useState('');
-  const [isChecked, setIsChecked] = useState(false);
-  const [isOpen, setIsOpen] = useState(false);
-
-  const options = [
-    { value: 'chocolate', label: 'Chocolate' },
-    { value: 'strawberry', label: 'Strawberry' },
-    { value: 'vanilla', label: 'Vanilla' }
-  ];
-
-  const menuOptions = [
-    {
-      label: 'Edit Board',
-      action: () => console.log('Action edit')
-    },
-    {
-      label: 'Delete Board',
-      color: MenuOptionColor.Danger,
-      action: () => console.log('Action delete')
-    }
-  ];
-
-  return (
-    <div style={{ padding: 16 }}>
-      <BaseMenu className="menu-preview" options={menuOptions} />
-      <BaseSelect options={options} value={option} onSelect={setOption} label="Label Test" />
-      <BaseCheckbox value={isChecked} label="Hovered" onChange={setIsChecked} />
-
-      <form style={{ display: 'flex', gap: '16px', flexDirection: 'column', maxWidth: '640px' }}>
-        <BaseTextField onChange={setText} value={text} label="Text Field" required placeholder="e.g Make cofee" />
-
-        <BaseTextarea
-          onChange={setDescription}
-          value={description}
-          label="Description"
-          placeholder="e.g. It’s always good to take a break. This 15 minute break will recharge the batteries a little."
-        />
-        <button type="submit">Submit</button>
-      </form>
-      <BaseModal isOpen={isOpen} renderHeader={() => <h1>Header Testing</h1>} onClose={setIsOpen}>
-        <p>Modal Content</p>
-      </BaseModal>
-      <BaseButton onClick={() => setIsOpen(!isOpen)}>Open Test Modal</BaseButton>
-
-      <BaseButton variant="contained" color="destructive">
-        Contained
-      </BaseButton>
-      <BaseButton variant="text" color="destructive">
-        Text
-      </BaseButton>
-      <BaseButton variant="outlined" color="destructive">
-        Outlined
-      </BaseButton>
-    </div>
-  );
+  return <BaseLayout>Contenido</BaseLayout>;
 }
 
 export default App;
